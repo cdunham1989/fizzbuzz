70b2351dd36bfcab7ca3a3f214b01ddbe249845d
Chris <cdunham1989@gmail.com>
2017-09-18 13:49:10 +0100

    code refactored and test added

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -20,4 +20,8 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed multiple of 3 and 5' do
     expect(fizzbuzz(-15)).to eq 'fizzbuzz'
   end
+
+  it 'returns "4" when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
 end
