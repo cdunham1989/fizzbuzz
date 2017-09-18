b28f0ac71743d5d001770251329f103f19957ea8
Chris <cdunham1989@gmail.com>
2017-09-18 16:16:26 +0100

    Fizzbuzz test working

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -18,4 +18,12 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(550)).to eq 'buzz'
   end
 
+  it 'returns "fizzbuzz" when multiples of 15' do
+    expect(fizzbuzz(90)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "fizzbuzz" when 0' do
+    expect(fizzbuzz(0)).to eq 'fizzbuzz'
+  end
+
 end
