8d843774ff470d7c98a569fe3a81f7bc56017331
Chris <cdunham1989@gmail.com>
2017-09-18 13:37:34 +0100

    fizzbuzz and rspec working

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+
+  if number % 3 == 0 && number % 5 == 0
+    return "fizzbuzz"
+  elsif number % 3 == 0
+    return "fizz"
+  elsif number % 5 == 0
+    return "buzz"
+  elsif number.string?
+    return "Invalid input"
+  else
+    return number
+  end
+
+end
